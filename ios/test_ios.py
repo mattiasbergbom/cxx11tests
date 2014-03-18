8d39edd2a638b3f6b5ab17c07e2156beda759fa7
Mattias Bergbom <mattias@swarmplanet.com>
2014-03-18 10:47:19 +0100

    Returning exit code of xcodebuild.

--- ios/test_ios.py
+++ ios/test_ios.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.Popen( 'xcodebuild', shell=True, cwd = os.path.dirname( os.path.realpath( __file__ ) ) ).wait()
+    retcode = subprocess.Popen( 'xcodebuild', shell=True, cwd = os.path.dirname( os.path.realpath( __file__ ) ) ).wait()
+    sys.exit( retcode )
